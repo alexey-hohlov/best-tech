4123e0544d760262de07ad77a01ae68e1d29d043
Alexey Hohlov <alexey.hohl0v@yandex.ru>
2024-08-23 00:15:17 +0700

    configure vue-router for deploy

--- src/router.ts
+++ src/router.ts
@@ -1,8 +1,4 @@
-import {
-  createWebHistory,
-  createRouter,
-  createWebHashHistory,
-} from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router';
 import {
   HomePage,
   CartPage,
