88ce9858e232473343cd262b59f4a6b822c4158a
Alexey Hohlov <alexey.hohl0v@yandex.ru>
2024-08-23 17:46:16 +0700

    fix: footer content for mobile

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -6,13 +6,15 @@ import { GitHubIcon } from '@/assets';
   <footer
     class="backdrop-blur-md bg-white/50 h-16 flex justify-center items-center gap-5"
   >
-    <div>Built by <span class="text-sky-400">Alexey Hohlov</span> ©2024</div>
+    <div class="hidden md:block">
+      Built by <span class="text-sky-400">Alexey Hohlov</span> ©2024
+    </div>
     <a
       class="flex items-center gap-2"
       href="https://github.com/alexey-hohlov"
       target="_blank"
     >
-      <GitHubIcon class='size-8'/>
+      <GitHubIcon class="size-8" />
       <span class="text-sky-400">alexey-hohlov</span>
     </a>
   </footer>
